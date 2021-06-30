226e944bd47362676a88929e0506a4b85371514c
Fgarciash <felix.garciash@gmail.com>
2021-06-29 21:41:39 -0400

    Versión 2

--- static/js/main.js
+++ static/js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' The alga is:  ' + data);
+                $('#result').text(' The seaweed is:  ' + data);
                 console.log('So does this');
             },
         });
